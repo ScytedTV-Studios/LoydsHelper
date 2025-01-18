2798f7099d3e4989992d8ac29f3398eb99dd8761
Loyd Osborne <loyd.osborne@scyted.tv>
2025-01-18 06:22:27 -0700

    Create welcome.js

---
+++ modules/servers/the-green-room/welcome.js
@@ -0,0 +1,60 @@
+client.on('guildMemberAdd', async member => {
+
+    const welcomedMembers = new Set();
+
+    try {
+
+      if (member.guild.id !== '1237187833324638209') return;
+
+      const systemChannel = member.guild.systemChannel;
+      if (!systemChannel) return console.error('System channel not found!');
+
+      if (welcomedMembers.has(member.id)) {
+        return console.log("Member has already been welcomed.");
+      }
+
+      const welcomeEmbed = new EmbedBuilder()
+        .setDescription(`${member} just joined the server, welcome!`)
+        .setColor('#FEEA3B');
+
+      const welcomeButton = new MessageButton()
+        .setCustomId(member.id) 
+        .setLabel('Welcome')
+        .setStyle('PRIMARY');
+
+      const row = new MessageActionRow().addComponents(welcomeButton);
+
+      const welcomeMessage = await systemChannel.send({ embeds: [welcomeEmbed], components: [row] });
+
+      welcomedMembers.add(member.id); 
+
+      const usersClicked = new Set(); 
+
+      const filter = i => i.customId === member.id; 
+
+      const collector = welcomeMessage.createMessageComponentCollector({ filter, time: 3600000 }); 
+
+      collector.on('collect', async interaction => {
+        try {
+          interaction.deferUpdate(); 
+          console.log(`Button clicked by: ${interaction.user.tag}`);
+          if (usersClicked.has(interaction.user.id)) return; 
+          const welcomeMessage = `**${interaction.user}** welcomes you, ${member}!`;
+          await systemChannel.send(welcomeMessage);
+          usersClicked.add(interaction.user.id); 
+        } catch (error) {
+          console.error('Error processing button click:', error);
+        }
+      });
+
+      setTimeout(() => {
+        row.components.forEach(button => {
+          button.setDisabled(true);
+        });
+        welcomeMessage.edit({ components: [row] });
+        collector.stop();
+      }, 3600000);
+    } catch (error) {
+      console.error('Error handling guildMemberAdd event:', error);
+    }
+  });
