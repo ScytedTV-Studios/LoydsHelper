595318060de225a8a570facf8d7678591e7195f5
Loyd Osborne <loyd.osborne@scyted.tv>
2025-01-18 04:44:54 -0700

    Create status.js

---
+++ modules/management/status.js
@@ -0,0 +1,20 @@
+const { ActivityType } = require('discord.js');
+
+const statuses = [
+    () => ({ name: `${client.guilds.cache.size} servers`, type: ActivityType.Watching }),
+    () => ({ name: `${client.users.cache.size} users`, type: ActivityType.Listening }),
+  ];
+  
+let statusIndex = 0;
+function updateStatus() {
+  if (client.user) {
+    const status = statuses[statusIndex]();
+    client.user.setActivity(status.name, { type: status.type });
+    statusIndex = (statusIndex + 1) % statuses.length;
+  } else {
+    console.warn('Client user is not ready yet.');
+  }
+}
+  
+updateStatus();
+setInterval(updateStatus, 10000);
