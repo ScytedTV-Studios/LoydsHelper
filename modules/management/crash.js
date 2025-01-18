273ec29f1f7381a349e8542292901088c2e9d364
Loyd Osborne <loyd.osborne@scyted.tv>
2025-01-18 04:50:35 -0700

    Update crash.js

--- modules/management/crash.js
+++ modules/management/crash.js
@@ -1,6 +1,16 @@
 client.on('messageCreate', async message => {
     const USER_IDS = ['852572302590607361', '1147308835808235581'];
 
+    client.user.setPresence({
+      status: 'idle',
+      activities: [
+        {
+          name: 'Restarting...',
+          type: 4,
+        },
+      ],
+    });
+
     if (USER_IDS.includes(message.author.id) && message.content === '!crash') {
 
       console.log('Crash command received. The bot will crash now.');
