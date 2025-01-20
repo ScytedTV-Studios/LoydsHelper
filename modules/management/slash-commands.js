27bb4f54c7e6563fe8d2f31fd46189d5e6939c68
Loyd Osborne <loyd.osborne@scyted.tv>
2025-01-20 13:46:43 -0700

    Update slash-commands.js

--- modules/management/slash-commands.js
+++ modules/management/slash-commands.js
@@ -38,6 +38,98 @@ const commands = [
                 type: 1
             }
         ]
+    },
+    {
+        "name": "ban",
+        "description": "Ban a user from the server",
+        "options": [
+            {
+                "type": 6,
+                "name": "user",
+                "description": "The user to ban",
+                "required": true
+            },
+            {
+                "type": 3,
+                "name": "reason",
+                "description": "Reason for the ban",
+                "required": false
+            }
+        ]
+    },
+    {
+        "name": "kick",
+        "description": "Kick a user from the server",
+        "options": [
+            {
+                "name": "user",
+                "description": "The user to kick",
+                "type": 6,
+                "required": true
+            },
+            {
+                "name": "reason",
+                "description": "The reason for the kick",
+                "type": 3,
+                "required": false
+            }
+        ]
+    },
+    {
+        "name": "timeout",
+        "description": "Timeout a user for a specified duration",
+        "options": [
+            {
+                "name": "user",
+                "description": "The user to timeout",
+                "type": 6,
+                "required": true
+            },
+            {
+                "name": "time",
+                "description": "The duration of the timeout",
+                "type": 3,
+                "required": true
+            },
+            {
+                "name": "reason",
+                "description": "The reason for the timeout",
+                "type": 3,
+                "required": false
+            }
+        ]
+    },
+    {
+        "name": "remove",
+        "description": "Remove various restrictions from a user",
+        "options": [
+            {
+                "name": "timeout",
+                "description": "Remove a timeout from a user",
+                "type": 1, 
+                "options": [
+                    {
+                        "name": "user",
+                        "description": "The user to remove the timeout from",
+                        "type": 6,
+                        "required": true
+                    }
+                ]
+            },
+            {
+                "name": "ban",
+                "description": "Remove a ban from a user by user ID",
+                "type": 1, 
+                "options": [
+                    {
+                        "name": "user",
+                        "description": "The user ID to remove the ban from",
+                        "type": 3,
+                        "required": true
+                    }
+                ]
+            }
+        ]
     }
 ];
 
