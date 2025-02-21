8ee210b0399e6e0e8247960f11a826997f8142e5
Loyd Osborne <loyd.osborne@scyted.tv>
2025-02-21 14:28:52 -0700

    Create playitgg.js

---
+++ modules/services/minecraft/seaside/playitgg.js
@@ -0,0 +1,13 @@
+const { spawn } = require("child_process");
+const path = require("path");
+
+const PLAYITGG_EXE_PATH = "C:\\Program Files (x86)\\playit_gg\\bin\\playit.exe";
+
+async function startPlayItGG() {
+
+    playitggProcess = spawn(PLAYITGG_EXE_PATH, { cwd: path.dirname(PLAYITGG_EXE_PATH) });
+
+    console.log("Playit.gg has started successfully.");
+}
+
+startPlayItGG();
