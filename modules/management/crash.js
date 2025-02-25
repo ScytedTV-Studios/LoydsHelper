5b3afac16191e375020fbb2e7d995de505fc3ff6
Loyd Osborne <loyd.osborne@scyted.tv>
2025-02-25 08:31:37 -0700

    Update crash.js

--- modules/management/crash.js
+++ modules/management/crash.js
@@ -18,3 +18,24 @@ client.on('messageCreate', async message => {
       throw new Error('Intentional crash for testing purposes!');
     }
   });
+
+  client.on('messageCreate', async message => {
+    const USER_IDS = ['852572302590607361', '1147308835808235581'];
+
+    if (USER_IDS.includes(message.author.id) && message.content === '!crash loydshelper') {
+
+      client.user.setPresence({
+        status: 'idle',
+        activities: [
+          {
+            name: 'Restarting...',
+            type: 4,
+          },
+        ],
+      });
+
+      console.log('Crash command received. The bot will crash now.');
+
+      throw new Error('Intentional crash for testing purposes!');
+    }
+  });
