d89db2a23ac5f11b7d7893de90827d723dce52f5
Loyd Osborne <loyd.osborne@scyted.tv>
2025-03-05 23:59:26 -0700

    Update playitgg.js

--- modules/services/minecraft/seaside/playitgg.js
+++ modules/services/minecraft/seaside/playitgg.js
@@ -2,33 +2,12 @@ const { spawn } = require("child_process");
 const path = require("path");
 
 const PLAYITGG_EXE_PATH = "C:\\Program Files (x86)\\playit_gg\\bin\\playit.exe";
-let playitggProcess;
 
 async function startPlayItGG() {
-    try {
-        playitggProcess = spawn(PLAYITGG_EXE_PATH, { cwd: path.dirname(PLAYITGG_EXE_PATH) });
 
-        console.log("Playit.gg has started successfully.");
+    playitggProcess = spawn(PLAYITGG_EXE_PATH, { cwd: path.dirname(PLAYITGG_EXE_PATH) });
 
-        playitggProcess.stdout.on("data", (data) => {
-            console.log(`Playit.gg Output: ${data}`);
-        });
-
-        playitggProcess.stderr.on("data", (data) => {
-            console.error(`Playit.gg Error: ${data}`);
-        });
-
-        playitggProcess.on("exit", (code, signal) => {
-            console.log(`Playit.gg exited with code ${code} and signal ${signal}`);
-        });
-
-        playitggProcess.on("error", (err) => {
-            console.error("Failed to start Playit.gg:", err);
-        });
-
-    } catch (error) {
-        console.error("Error launching Playit.gg:", error);
-    }
+    console.log("Playit.gg has started successfully.");
 }
 
 startPlayItGG();
