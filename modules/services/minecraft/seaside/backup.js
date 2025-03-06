ae9e5502177d6fd3adb1ea6d6974d19400044c64
Loyd Osborne <loyd.osborne@scyted.tv>
2025-03-06 00:00:08 -0700

    Update backup.js

--- modules/services/minecraft/seaside/backup.js
+++ modules/services/minecraft/seaside/backup.js
@@ -1,20 +1,20 @@
-const axios = require('axios');
+// const axios = require('axios');
 
-async function createBackupIfNeeded() {
-    try {
-        const response = await axios.get('http://localhost:8000/seaside/data/online.json');
-        const data = response.data;
+// async function createBackupIfNeeded() {
+//     try {
+//         const response = await axios.get('http://localhost:8000/seaside/data/online.json');
+//         const data = response.data;
 
-        if (Array.isArray(data) && data.length > 0) {
-            // console.log('Contents detected in online.json. Running createBackup...');
-            createBackup();
-        } else {
-            // console.log('No contents in online.json. Skipping createBackup.');
-        }
-    } catch (error) {
-        console.error('Error fetching online.json:', error.message);
-    }
-}
+//         if (Array.isArray(data) && data.length > 0) {
+//             // console.log('Contents detected in online.json. Running createBackup...');
+//             createBackup();
+//         } else {
+//             // console.log('No contents in online.json. Skipping createBackup.');
+//         }
+//     } catch (error) {
+//         console.error('Error fetching online.json:', error.message);
+//     }
+// }
 
-createBackupIfNeeded();
-setInterval(createBackupIfNeeded, 1200000);
+// createBackupIfNeeded();
+// setInterval(createBackupIfNeeded, 1200000);
